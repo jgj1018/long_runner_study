4a139f3a13dd144b61137c52a0bacba679b18368
youngho.kim <jgj1018@gmail.com>
2017-10-15 21:29:11 +0900

    [20171015] Singleton revision

--- Design Pattern/Kim/src/Singleton/SingletonPatternDemo.kt
+++ Design Pattern/Kim/src/Singleton/SingletonPatternDemo.kt
@@ -2,6 +2,12 @@ package Singleton
 
 fun main(args: Array<String>) {
 
-    val obj = SingleObject.getInstance()
+    val obj = SingleObject.Single.getInstance()
     obj.showMessage()
+
+    val obj2 = SingleObject.Single.getInstance()
+    obj2.showMessage()
+
+    SingleObject2.showMessage()
+    SingleObject2.showMessage()
 }
