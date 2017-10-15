72378334e7aeb73b6522c3249dd7dc5ad6280243
youngho.kim <jgj1018@gmail.com>
2017-10-15 21:29:11 +0900

    [20171015] Singleton revision

--- Design Pattern/Kim/src/Singleton/SingleObject.kt
+++ Design Pattern/Kim/src/Singleton/SingleObject.kt
@@ -1,10 +1,20 @@
 package Singleton
 
+// first way
 class SingleObject private constructor() {
 
-    companion object {
-        fun getInstance() = SingleObject()
+    object Single {
+        private val instance = SingleObject()
+        fun getInstance() : SingleObject = instance
     }
+
+    fun showMessage() {
+        println("Hello World!"+this.hashCode())
+    }
+}
+
+// second and more simple way
+object SingleObject2 {
 
     fun showMessage() {
         println("Hello World!"+this.hashCode())
